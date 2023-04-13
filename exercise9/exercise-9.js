6c12add54c278ca6662530bcd9331da15bf6a448
Gabriele <gabriele250901@outlook.it>
2023-04-13 14:29:13 +0200

    exercise 9 changes

--- exercise9/exercise-9.js
+++ exercise9/exercise-9.js
@@ -8,8 +8,6 @@ let keys = Object.keys(person);
 console.log(keys);
 
 // Print values of person using Object.keys
-let entry  = Object.entries(person);
-console.log(entry);
-
-/* come vedi sopra ho provato ad usare keys , ma per stampare in coppia come chiede 
-l'esercizio credo sia piu` opportuno usare entries */
+keys.forEach (function(key) {
+ console.log(`${key}: ${person[key]}`)
+});
