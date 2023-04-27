07604dc39ad55f8505f1318003d5d13c0bbeb392
Gabriele <gabriele250901@outlook.it>
2023-04-27 16:45:12 +0200

    done

---
+++ exercise38/exercise38.js
@@ -0,0 +1,29 @@
+const isLogged = true;
+
+function firstPromise(isLogged){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+        if(isLogged === true){
+            resolve(Math.random());
+        }else{
+            reject(new Error("User is not logged!"))
+        }
+    },500)
+})
+}
+function secondPromise(isLogged){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            if(isLogged > 0.5){
+                resolve(`{name: "John", age: 24}`)
+            }else{
+                reject(new Error("something went wrong!"))
+            }
+        },500)
+    })
+}
+
+firstPromise(isLogged)
+.then(secondPromise)
+.then((val)=> console.log(val))
+.catch((err)=> console.error(err))
