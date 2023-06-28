be108ed0951d501b3e837e1be98eff6e02dce89f
Gabriele <gabriele250901@outlook.it>
2023-06-28 12:41:35 +0200

    done

---
+++ exercise2/outputMess.js
@@ -0,0 +1,5 @@
+function outputMess(message) {
+  console.log(`Your message is: ${message}`);
+}
+
+module.exports = outputMess;
---
+++ exercise2/script.js
@@ -0,0 +1,3 @@
+const outputMessage = require("./outputMess");
+
+outputMessage("Exercise2, Gabriele Monti");
